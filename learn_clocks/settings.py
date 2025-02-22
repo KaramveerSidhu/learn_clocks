6f4d3f861c61b21f293e49e77a2651ac612b17c3
KaramveerSidhu <karamveersidhu14@gmail.com>
2025-02-21 16:59:05 -0800

    feat: set DEBUG False

--- learn_clocks/settings.py
+++ learn_clocks/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-utgrulb1g8qvf+t393v^7313+qod9i6tou-12%l3+cma^kya%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
