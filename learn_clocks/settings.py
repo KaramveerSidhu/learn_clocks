09f8da22b8227c03b737907deb6e86e42a0f44fe
KaramveerSidhu <karamveersidhu14@gmail.com>
2025-02-21 17:59:31 -0800

    fix: fix deployment

--- learn_clocks/settings.py
+++ learn_clocks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-utgrulb1g8qvf+t393v^7313+qod9i6tou-12%l3+cma^kya%l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app' "learn-clocks.vercel.app", "learn-clocks-karamveersidhus-projects.vercel.app", "learn-clocks-karamveersidhu-karamveersidhus-projects.vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
